24ef17d3f3d77e8eb1a536cbd370933b01acf8c9
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2023-03-28 21:45:07 +0100

    added axios  to handel form submissions

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import Title from "./Title.jsx";
+// added axios to handle form submission
+import axios from "axios";
+
 export default function Contact() {
+  // add event listener to form
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_APP_FORM_URL,
+        formData
+      );
+      console.log(response); // log the response for debugging purposes
+      // show a success message to the user
+    } catch (error) {
+      console.error(error); // log the error for debugging purposes
+      // show an error message to the user
+    }
+  };
   return (
     <div className="flex flex-col mb-10 mx-auto">
       <div className="flex justify-center items-center">
         <form
-          // I got this form from https://getform.io/ sign up for a free account and create a form
-          // then copy the url and paste it in a .env file in the root of your project
-          // then add the .env file to your .gitignore file
-          //then in your .env file add VITE_APP_FORM_URL=your-form-url
-          //then in your code you can use import.meta.env.VITE_APP_FORM_URL
-          action={import.meta.env.VITE_APP_FORM_URL}
           method="POST"
           className="flex flex-col w-full md:w-1/2"
+          onSubmit={handleSubmit} // add event listener
         >
-        <div className="mt-2">
-          <Title>Contact</Title>
+          <div className="mt-2">
+            <Title>Contact</Title>
           </div>
           <input
             type="text"
